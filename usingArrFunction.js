0d1f0c4e728b34458a175f3c4bafbbd33e59456f
eewhy <40720321+eewhy@users.noreply.github.com>
2018-10-25 13:11:27 -0400

    Update usingArrFunction.js

--- usingArrFunction.js
+++ usingArrFunction.js
@@ -5,8 +5,7 @@ const superHeroes = [
 ]
 
 const secretIdentity = superHeroes.map(function(revealArray, heroes) {
-  //return revealArray[0] + ' is ' + revealArray[1] + '.';
   return revealArray.join('is');
-}, '');
+});
 
-//console.log(secretIdentity.join('\n'));
+console.log(secretIdentity.join('\n'));
